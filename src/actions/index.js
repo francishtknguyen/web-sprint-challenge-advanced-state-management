13c9361c401522ca79ff511e1c8cc3c3a8340f2a
Francis Nguyen <francishtknguyen@gmail.com>
2021-04-16 12:29:16 -0500

    finished mvp

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,29 @@ import axios from 'axios';
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
 //3. Add a standard action that allows us to set the value of the error message slice of state.
+export const FETCH_SMURFS_START = 'FETCH_SMURFS_START'
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS'
+export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL'
+export const ADD_SMURF = 'ADD_SMURF'
+export const ADD_ERROR = 'ADD_ERROR'
+
+export const addError = (error) => {
+    return({type:ADD_ERROR, payload: error})
+}
+export const addSmurf = (features) => {
+    return({type:ADD_SMURF, payload:features})
+}
+export const fetchSmurfs = () => {
+    return (dispatch) => {
+        dispatch({type:FETCH_SMURFS_START})
+        axios.get('http://localhost:3333/smurfs')
+        .then((resp)=>{
+            console.log(resp)
+            dispatch({type:FETCH_SMURFS_SUCCESS, payload:resp.data})
+        })
+        .catch((err) => {
+            console.log(err)
+            dispatch({type:FETCH_SMURFS_FAIL, payload:err})
+        })
+    }
+}
--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import {connect} from 'react-redux'
+import {addError, addSmurf} from '../actions'
+
 
 const AddForm = (props) => {
     const [state, setState] = useState({
@@ -13,16 +16,21 @@ const AddForm = (props) => {
             ...state,
             [e.target.name]:e.target.value
         });
+        
     }
 
     const handleSubmit = e => {
         e.preventDefault();
         if (state.name === "" || state.position === "" || state.nickname === "") {
-            errorMessage = "Name, position and nickname fields are required.";
+            props.addError("Name, position and nickname fields are required.");
+        }
+        else{
+            props.addError("")
+            props.addSmurf(state);
         }
     }
 
-    const errorMessage = "";
+    // const errorMessage = "";
 
     return(<section>
         <h2>Add Smurf</h2>
@@ -44,14 +52,21 @@ const AddForm = (props) => {
                 <textarea onChange={handleChange} value={state.description} name="description" id="description" />
             </div>
             {
-                errorMessage && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {errorMessage}</div>
+                props.error && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {props.error}</div>
             }
             <button>Submit Smurf</button>
         </form>
     </section>);
 }
 
-export default AddForm;
+const mapStateToProps = (state) => {
+    return{
+        error: state.error,
+    }
+}
+
+export default connect(mapStateToProps,{addError, addSmurf})(AddForm);
+
 
 //Task List:
 //1. Connect the errorMessage, setError and addSmurf actions to the AddForm component.
